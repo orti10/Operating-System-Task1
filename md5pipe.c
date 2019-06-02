44cf91eddddec803c4abc97887a5651d5fdc5215
tomerr1212 <tomermaabari@gmail.com>
2019-06-02 10:56:14 +0300

    checking

--- md5pipe.c
+++ md5pipe.c
@@ -1,34 +1,72 @@
+#include <string.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/uio.h>
 #include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include "md5.h"
+using std::cout; using std::endl;
+#define BUFFER_SIZE 20
+#define BUFFER_SIZE32 10
+#define READ_END 0
+#define WRITE_END 1
+
 
 int main(void)
 {
-        int     fd[2], nbytes;
-        pid_t   childpid;
-        char    string[] = "Hello, world! I'm the son and this my message!\n";
-        char    readbuffer[80];
 
-        pipe(fd);   // piping fd[0] & fd[1]
-
-        if((childpid = fork()) == -1)   // here we create a SON process
-        {
-                perror("fork");
-                exit(1);
-        }
-
-         if(childpid == 0)    // child process
-    {
-            write(fd[1], string, (strlen(string)+1)); #C1
-            printf("Verification : Message was sent successfully by the SON!\n"); #C2
+  char write_msg[BUFFER_SIZE];
+  char write_msg2[BUFFER_SIZE32];
+  int fd[2];
+  int fd2[2];
+  int  pid;
+  if (pipe(fd) == -1) {
+    fprintf(stderr, "Pipe failed");
+    return 1;
+  }
+  if (pipe(fd2) == -1) {
+    fprintf(stderr, "Pipe failed");
+    return 1;
+  }
+  pid = fork();
+ 
+  if (pid < 0) {
+    fprintf(stderr, "Fork failed");
+    return 1;
+  }
+ 
+  else if (pid>0){
+    close(fd[READ_END]);
+    printf("plain text: ");
+    scanf("%20s",write_msg);
+    write(fd[WRITE_END],write_msg,20);    
+    close(fd[WRITE_END]);
+    wait(NULL);
+    close(fd2[WRITE_END]);
+    read(fd2[READ_END],write_msg2,32);
+    close(fd2[READ_END]);
+    if(strlen(write_msg2)==32){
+            printf("encrypted by process %d:%s\n",getpid(),write_msg2);
+            kill(pid,SIGKILL);
+    }
+    else{
             exit(0);
     }
-    else    // father process
-    {
-            /* Read in a string from the pipe */
-            nbytes = read(fd[0], readbuffer, sizeof(readbuffer)); #F1
-            printf("I'm the father and I received that string: %s", readbuffer); F2
     }
+ 
+  else {
+    close(fd[WRITE_END]);
+    read(fd[READ_END],write_msg,20);
+    std::string s=md5(write_msg);
+    close(fd[READ_END]);
+    close(fd2[READ_END]);
+    write(fd2[WRITE_END],s.c_str(),32);
+    close(fd2[WRITE_END]);
+    exit(0);
+  }
 
-        return(0);
+  return 0;
 }
